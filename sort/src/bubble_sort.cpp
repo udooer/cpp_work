382a0b0e68b8794eec69d03000f295049d359e91
shane <hunghsuyong114shane@gmail.com>
2020-10-22 18:00:35 +0800

    insertion sort and bubble sort test

---
+++ sort/src/bubble_sort.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+vector<double> randVector(int, int, int);
+void printArray(double *first, int size){
+    for(int i=0;i<size;i++)
+        cout<<*(first+i)<<' ';
+    cout<<'\n';
+    return;
+}
+void bubbleSort(vector<double> &v){
+	for(int i=v.size();i>0;i--)
+		for(int j=1;j<=i;j++)
+			if(v[j-1]>v[j]){
+				swap(v[j-1], v[j]);
+			}
+}
+int main(){
+	vector<double> v_unsorted = randVector(20, 0, 100);
+	cout<<"before sorted:\n";
+	printArray(v_unsorted.data(), v_unsorted.size());
+	bubbleSort(v_unsorted);
+	cout<<"after sorted:\n";
+	printArray(v_unsorted.data(), v_unsorted.size());
+}
---
+++ sort/src/insertion_sort.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+vector<double> randVector(int, int, int);
+void printArray(double *first, int size){
+    for(int i=0;i<size;i++)
+        cout<<*(first+i)<<' ';
+    cout<<'\n';
+    return;
+}
+void insertionSort(vector<double> &v){
+    for(int i=1;i<v.size();i++)
+        for(int j=i;j>0;j--){
+            if(v[j]>v[j-1])
+                break;
+            else
+                swap(v[j], v[j-1]);
+        }
+    return; 
+}
+int main(){
+    vector<double> v_unsorted;
+    v_unsorted = randVector(20, 0, 100);
+    cout<<"before sorted: \n";
+    printArray(v_unsorted.data(), v_unsorted.size());
+    insertionSort(v_unsorted);
+    cout<<"after sorted: \n";
+    printArray(v_unsorted.data(), v_unsorted.size());
+    return 0;
+}
